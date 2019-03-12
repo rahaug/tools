fe47566107b27e8d091167f4598e23c08aec22a0
Rolf Haug <rolf@rah.no>
2019-03-12 10:15:23 +0100

    Create php-linter.sh

---
+++ php-linter.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Handy command to check all PHP files for syntax errors when upgrading PHP version on a server
+
+# Store this folder in your project root
+# run the bash script (tools/php-linter.sh)
+
+# It will recursively check all PHP files from the root for syntax errors (deprecated functions, etc.)
+find $(dirname "${BASH_SOURCE[0]}")/.. -name '*.php' | xargs -n1 /usr/bin/php -l
