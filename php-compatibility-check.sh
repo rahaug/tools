b731a98266c6b68639d992dfba216df15497280b
Rolf Haug <rolf@rah.no>
2019-03-12 21:46:09 +0100

    Renamed bash script

--- php-compatibility-check.sh
+++ php-compatibility-check.sh
@@ -3,7 +3,7 @@
 # Handy command to check all PHP files for syntax errors when upgrading PHP version on a server
 
 # Store this folder in your project root
-# run the bash script (tools/php-linter.sh)
+# run the bash script (tools/php-compatibility-check.sh)
 
 # It will recursively check all PHP files from the root for syntax errors (deprecated functions, etc.)
 find $(dirname "${BASH_SOURCE[0]}")/.. -name '*.php' | xargs -n1 /usr/bin/php -l
